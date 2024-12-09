115bc69d6af9d38e2aea48dfe76b868ec8480ab6
xiao fu <7938813+xiaofu2024@users.noreply.github.com>
2024-12-09 18:25:20 +0800

    下载

--- lib/vc/FullVideoPlayer.dart
+++ lib/vc/FullVideoPlayer.dart
@@ -29,6 +29,7 @@ class _FullvideoplayerState extends State<Fullvideoplayer> {
     Uri? uri;
     try {
       uri = Uri.parse(widget.message.uri);
+      //uri = Uri.parse("https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8");
       _videoPlayerController = VideoPlayerController.networkUrl(uri);
       await _videoPlayerController.initialize();
       setState(() {
