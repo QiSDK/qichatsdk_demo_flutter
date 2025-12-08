4ef1068f67a152df816e1692f06fc0a1ebc24058
xiao fu <7938813+xiaofu2024@users.noreply.github.com>
2025-12-08 22:03:41 +0800

    good

--- lib/manager/global_chat_manager.dart
+++ lib/manager/global_chat_manager.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:qichatsdk_flutter/qichatsdk_flutter.dart';
+import 'package:flutter_qichat_sdk/flutter_qichat_sdk.dart';
 import 'package:fixnum/fixnum.dart';
 import '../Constant.dart';
 import 'unread_manager.dart';
